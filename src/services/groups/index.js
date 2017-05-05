100b34d511676f4f81ad6863863560cecc095903
soier8 <brunnys_2008@hotmail.com>
2017-05-05 16:38:52 -0300

    adicionado cidades e grupos de bh

--- src/services/groups/index.js
+++ src/services/groups/index.js
@@ -1,40 +1,159 @@
 export default {
   cities: [
     {
-      name: 'Betim',
-      banner: 'betim.jpg',
+      name: 'Belo Horizonte',
+      banner: '',
       groups: [
         {
-          name: '83 GEOSC',
-          meetings: 'Sabados 15h',
+          name: 'GE DO AR PADRE EUSTÁQUIO - 07/MG ',
+          meetings: 'Reuniões Sábado',
           banner: '',
-          address: 'Pq de exposicoes',
-          phone: '28903290'
+          address: 'RUA OCIDENTE,39 - PADRE EUSTÁQUIO',
+          phone: 'Telefone: (31) 3411-1148'
         },
         {
-          name: '97 GECAN',
-          meetings: 'Sabados 15h',
+          name: 'GE P. DAS MANGABEIRAS - 21/MG',
+          meetings: 'Reuniões Sábado das 14:30 às 17:00',
           banner: '',
-          address: 'Pq de exposicoes',
-          phone: '28903290'
+          address: 'Rua Caraça, 900 - Bairro: Serra - Belo Horizonte',
+          phone: 'Cel.: Fernanda Soares - (31) 9 8817 6256'
+        },
+        {
+          name: 'GE ANTÔNIO MOURÃO GUIMARÃES - 23/MG ',
+          meetings: 'Reuniões sábados a tarde',
+          banner: '',
+          address: 'R JURAMENTO,1464 - BALEIA',
+          phone: 'Telefone: (31) 99978-4443'
+        },
+        {
+          name: 'GE UIRAPURU - 34/MG ',
+          meetings: '',
+          banner: '',
+          address: 'RUA LEOPOLDO GOMES,,1380 - VERA CRUZ',
+          phone: 'Telefone: (31) 3488-2171'
+        },
+        {
+          name: 'GE SANTO AGOSTINHO - 39/MG ',
+          meetings: 'Reuniões sábado a tarde',
+          banner: '',
+          address: 'R ARAGUARI,915 - SANTO AGOSTINHO',
+          phone: 'Telefone: (31) 3337-5925'
+        },
+        {
+          name: 'GE LAGOA DO NADO - 46/MG ',
+          meetings: 'Reuniões sábado a tarde',
+          banner: '',
+          address: 'Rua Aureliano Lessa,416 - Liberdade',
+          phone: 'Telefone: (31) 9995-11523'
+        },
+        {
+          name: 'GE DUQUE DE CAXIAS - 52/MG ',
+          meetings: 'Reuniões sábado a tarde',
+          banner: '',
+          address: 'Av Afonso Pena,1377 - Centro',
+          phone: 'Telefone: (31) 98455-4996'
+        },
+        {
+          name: 'GE PARQUE DAS ÁGUAS - 98/MG ',
+          meetings: 'Reuniões sábado a tarde',
+          banner: '',
+          address: 'Avenida Ximango,809 - Flávio Marques Lisboa',
+          phone: ''
+        },
+        {
+          name: 'GE DO AR LANDELL DE MOURA - 101/MG ',
+          meetings: 'Reuniões sábado a tarde',
+          banner: '',
+          address: 'RUA MÁRIO MARTINS,53 - POMPÉIA',
+          phone: 'Telefone: Chefe Marta - 99313-3961'
+        },
+        {
+          name: 'GE JACQUES COSTEAU - 106/MG ',
+          meetings: 'Reuniões sábado a tarde',
+          banner: '',
+          address: 'Rua Augusto Jose dos Santos,366 - Betânia',
+          phone: 'Telefone: (31) 3277-4185'
+        },
+        {
+          name: 'GE CORONEL VICENTE TORRES JÚNIOR - 107/MG',
+          meetings: 'Reuniões sábado a tarde',
+          banner: '',
+          address: 'AV. WALDIR SOEIRO EMRICH - VIA DO MINÉRIO,1001 - MILIONÁRIOS',
+          phone: 'Telefone: (31) 9914-3286'
+        },
+        {
+          name: 'GE SÉRGIO VIEIRA DE MELLO - 109/MG',
+          meetings: 'Reuniões sábado a tarde ',
+          banner: '',
+          address: 'RUA ENEIDA,635 - COQUEIROS',
+          phone: 'Telefone: (31) 8519-8865'
+        },
+        {
+          name: 'GE VENDA NOVA - 113/MG',
+          meetings: 'Reuniões sábado a tarde',
+          banner: '',
+          address: 'Rua José Ferreira dos Santos,184 - Leticia',
+          phone: 'Telefone: (31) 3459-6614'
+        },
+        {
+          name: 'GE JARDIM BELMONTE - 117/MG ',
+          meetings: 'Reuniões sábado a tarde',
+          banner: '',
+          address: 'JORNALISTA ABRAHÃO SADI,380 - JARDIM BELMONTE',
+          phone: 'Telefone: (31) 3435-0156'
+        },
+        {
+          name: 'GE PARQUE PRIMEIRO DE MAIO - 118/MG',
+          meetings: 'Reuniões sábado a tarde',
+          banner: '',
+          address: 'PENÉLOPE,190 - PRIMEIRO DE MAIO',
+          phone: 'Telefone: Chefe Lau - (31) 99640-3978'
+        },
+        {
+          name: 'GE TAQUARIL - 135/MG',
+          meetings: 'Reuniões sábado a tarde',
+          banner: '',
+          address: 'MARIO MARTINS,477 - POMPEIA ',
+          phone: ''
+        },
+        {
+          name: 'GE NOVA FLORESTA - 139/MG',
+          meetings: 'Reuniões sábado a tarde',
+          banner: '',
+          address: '',
+          phone: ''
+        },
+        {
+          name: 'GE ALFERES TIRADENTES - 149/MG',
+          meetings: 'Reuniões sábado a tarde',
+          banner: '',
+          address: 'RUA PLATINA,580 - PRADO',
+          phone: 'Telefone: (31) 8612-0498'
+        },
+        {
+          name: 'GE CATÓLICO SÃO THIAGO - 188/MG',
+          meetings: 'Reuniões sábado a tarde',
+          banner: '',
+          address: 'Rua Guanabara,655 - Concórdia',
+          phone: 'Telefone: (31) 3442-2315'
         }
       ]
     },
     {
-      name: 'Smallvile',
+      name: 'Betim',
       banner: '',
       groups: [
         {
-          name: 'da serra',
-          meetings: '00h todo dia',
+          name: 'GRUPO ESCOTEIRO OLAVE SAINT CLAIR - 83/MG ',
+          meetings: 'Reuniões sábado a tarde',
           banner: '',
-          address: 'marte',
-          phone: '2890280'
+          address: 'RUA DO ROSÁRIO,1840 - INGÁ',
+          phone: 'Telefone: (31) 9254-5591'
         }
       ]
-    }
-    /* {
-      name: '',
+    },
+    {
+      name: 'Conceição do Mato Dentro',
       banner: '',
       groups: [
         {
@@ -45,6 +164,85 @@ export default {
           phone: ''
         }
       ]
-    } */
+    },
+    {
+      name: 'Contagem',
+      banner: '',
+      groups: [
+        {
+          name: '',
+          meetings: '',
+          banner: '',
+          address: '',
+          phone: ''
+        }
+      ]
+    },
+    {
+      name: 'Lagoa Santa',
+      banner: '',
+      groups: [
+        {
+          name: '',
+          meetings: '',
+          banner: '',
+          address: '',
+          phone: ''
+        }
+      ]
+    },
+    {
+      name: 'Mateus Leme',
+      banner: '',
+      groups: [
+        {
+          name: '',
+          meetings: '',
+          banner: '',
+          address: '',
+          phone: ''
+        }
+      ]
+    },
+    {
+      name: 'Nova Lima',
+      banner: '',
+      groups: [
+        {
+          name: '',
+          meetings: '',
+          banner: '',
+          address: '',
+          phone: ''
+        }
+      ]
+    },
+    {
+      name: 'Raposos',
+      banner: '',
+      groups: [
+        {
+          name: '',
+          meetings: '',
+          banner: '',
+          address: '',
+          phone: ''
+        }
+      ]
+    },
+    {
+      name: 'Sabará',
+      banner: '',
+      groups: [
+        {
+          name: '',
+          meetings: '',
+          banner: '',
+          address: '',
+          phone: ''
+        }
+      ]
+    }
   ]
 }
+
